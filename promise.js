fccddae5dcea95b7469f144a1006fed3e174bf0f
adiputrayasa <adiputrayasa09@gmail.com>
2022-09-19 23:07:37 +0800

    tugas promise

--- promise.js
+++ promise.js
@@ -1,7 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let nontonMarah = 0;
+let nontonTidakMarah = 0; 
+const promiseOutput = emosi => {
+  if (emosi === 'marah' || 'tidak marah') {
+      return promiseTheaterIXX().then((value) => {
+      return promiseTheaterVGC().then((value) => {
+        value.forEach((array) => {
+              if (array.hasil === 'marah') {
+                nontonMarah++;
+             
+              }
+              else if (array.hasil === 'tidak marah'){
+                nontonTidakMarah++;
+              }
+              else {
+                'error';
+              }
+        });
+        return nontonMarah
+
+     
+      }); 
+      });
+   
+
+  }
+}
+
+
 
 module.exports = {
   promiseOutput,
