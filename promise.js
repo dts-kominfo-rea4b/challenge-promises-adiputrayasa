28fb5ea963b220af743d06f065a219fcf310597c
adiputrayasa <adiputrayasa09@gmail.com>
2022-09-20 15:05:59 +0800

    Ada error

--- promise.js
+++ promise.js
@@ -1,33 +1,32 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-let nontonMarah = 0;
-let nontonTidakMarah = 0; 
-const promiseOutput = emosi => {
-  if (emosi === 'marah' || 'tidak marah') {
-      return promiseTheaterIXX().then((value) => {
-      return promiseTheaterVGC().then((value) => {
-        value.forEach((array) => {
-              if (array.hasil === 'marah') {
-                nontonMarah++;
-             
-              }
-              else if (array.hasil === 'tidak marah'){
-                nontonTidakMarah++;
-              }
-              else {
-                'error';
-              }
-        });
-        return nontonMarah
 
-     
-      }); 
-      });
-   
+//let jmlnonton = 0; cukup 1 variabel saja 
 
-  }
-}
+const promiseOutput =  async (emosi) => {
+  let jmlnonton = 0;
+  // if (emosi === 'marah' || 'tidak marah') { -> tidak perlu if karena kita langsung ambil hasil input nya
+  await promiseTheaterIXX().then( (value) => { 
+    value.forEach((array) => {
+      if (array.hasil === emosi) {
+        jmlnonton++;
+      }
+    });
+  });
+
+  await promiseTheaterVGC().then( (value) => { 
+    value.forEach((array) => {
+      if (array.hasil === emosi) {
+        jmlnonton++;
+      }
+    });
+  });
+
+  return jmlnonton;
+};
+
+
 
 
 
